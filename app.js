49ec5de3e6cf736e5681fd143fc46555a35512b9
Kritansh <kkritansh7@gmail.com>
2020-04-03 10:54:14 +0530

    changes

--- app.js
+++ app.js
@@ -57,8 +57,8 @@ if(process.env.NODE_ENV === 'production') {
 //---------------
 //Mongo URI
 dotenv.config();
-const mongoURI = "mongodb://localhost/stickman_game_project";
-// const mongoURI = "mongodb+srv://"+process.env.MLAB_USER+":"+process.env.MLAB_PASS+"@cluster0-kw5s2.mongodb.net/stickman?retryWrites=true&w=majority";
+// const mongoURI = "mongodb://localhost/stickman_game_project";
+const mongoURI = "mongodb+srv://"+process.env.MLAB_USER+":"+process.env.MLAB_PASS+"@cluster0-kw5s2.mongodb.net/stickman?retryWrites=true&w=majority";
 //Mongo connection
 mongoose
     .connect(mongoURI, { 
